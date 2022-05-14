de784819cc412c09094b356eee273a2a87100b64
AntonyIS <antony@ariyafinergy.com>
2022-05-14 19:41:06 +0300

    Adding serializer logic

---
+++ serializer/json/serializer.go
@@ -0,0 +1,27 @@
+package json
+
+import (
+	"encoding/json"
+
+	"github.com/AntonyIS/url_shortener/shortener"
+	"github.com/pkg/errors"
+)
+
+type Redirect struct{}
+
+func (*Redirect) Decode(input []byte) (*shortener.Redirect, error) {
+	redirect := &shortener.Redirect{}
+
+	if err := json.Unmarshal(input, redirect); err != nil {
+		return nil, errors.Wrap(err, "serlializer.Redirect.Decode")
+	}
+	return redirect, nil
+}
+
+func (*Redirect) Encode(input string) ([]byte, error) {
+	rawMsg, err := json.Marshal(input)
+	if err != nil {
+		return nil, errors.Wrap(err, "serlializer.Redirect.Decode")
+	}
+	return rawMsg, nil
+}
